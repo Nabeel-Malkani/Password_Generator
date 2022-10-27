12ef187b3b2e979636ba638b04023c0f2bc2ae7b
Nabeel Malkani <nabeelmalkani@gmail.com>
2022-10-27 12:46:09 +0500

    Initial Commit
    First init for files

---
+++ main.js
@@ -0,0 +1,20 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let alphaArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let symbolArray = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+
+let symbolpassword = alphaArray.concat(symbolArray);
+
+let numberpassword = alphaArray.concat(numArray);
+
+let password1 = ["0", "1", "2", "3", "4", "5", "6", "7"];
+let password = '';
+const generatePassword = () => {
+    password1.forEach(element => {
+        password += characters[Math.floor(Math.random() * characters.length)]
+    });
+    document.getElementById("rect1").innerText = password1
+}
